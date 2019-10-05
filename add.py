2ba99d861ec7f089804c1bd0f21acf4aab32135a
Nick Voulelis <njvoulelis@gmail.com>
2019-10-05 14:51:18 -0600

    I changed stuff for all files

---
+++ add.py
@@ -0,0 +1,5 @@
+def add():
+    x=1+2+3
+    return x
+
+print(add())
--- server.py
+++ server.py
@@ -5,10 +5,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout', methods=['POST'])    
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    count = int(request.form['apple']) + int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['blackberry'])
+    print('x'*120)
+    print(f"Charging {request.form['first_name'] + ' ' + request.form['last_name']} for {count} fruits")
+    return render_template("checkout.html",
+     numApple = request.form['apple'],
+     numStraw = request.form['strawberry'],
+     numRasp = request.form['raspberry'],
+     numBlack = request.form['blackberry'],
+     firstName = request.form['first_name'],
+     lastName = request.form['last_name'],
+     userId = request.form['student_id']
+     )
 
 @app.route('/fruits')         
 def fruits():
